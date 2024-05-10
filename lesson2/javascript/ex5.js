67ad9259b31870042e52fe3abc791c58b3f65cdb
K6 - Phuong <leuyenphuong128@gmail.com>
2024-05-11 00:10:31 +0700

    add more files

---
+++ lesson2/javascript/ex5.js
@@ -0,0 +1,23 @@
+// Lặp từ 1 tới 100
+for (let i = 1; i <= 100; i++){
+    console.log("Giá trị của i là: ", i);
+};
+
+// Nếu giá trị của vòng lặp chia hết cho 2, in “Số <i> là số chẵn”
+  for (let i = 1; i <= 100; i++){
+    if (i % 2 === 0){
+        console.log(i);
+    }
+  };
+
+// Nếu giá trị của vòng lặp không chia hết cho 2, in “Số <i> là số lẻ”
+for (let i = 1; i <= 100; i++){
+    if (i % 2 === 1){
+        console.log(i);
+    }
+  };
+
+  // In ra giá trị từ 12 đến 30
+for (let i=12; i<=30; i++){
+    console.log(i);
+}
---
+++ lesson2/javascript/ex7.js
@@ -0,0 +1,9 @@
+
+for (let i = 1; i <= 100; i++){
+    if (i % 4 === 1){
+    console.log(i);
+   }
+}
+
+
+    
