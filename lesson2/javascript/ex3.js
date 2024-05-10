81755e91f9b7270647477771ef29da9a4b79b757
K6 - Phuong <leuyenphuong128@gmail.com>
2024-05-10 22:38:06 +0700

    Dieu kien

---
+++ lesson2/javascript/ex3.js
@@ -0,0 +1,17 @@
+// a.Khai báo một hằng số với giá trị kiểu Number bất kỳ.
+// b.In ra dòng sau ở console:
+// i.
+const myNumber = 128;
+
+if (myNumber > 0){
+  console.log("Giá trị bạn nhập là số dương")
+};
+
+
+if (myNumber < 0){
+    console.log("Giá trị bạn nhập là số âm")
+};
+
+if (myNumber === 0){
+    console.log("Giá trị của bạn nhập là số 0")
+};
---
+++ lesson2/javascript/ex4.js
@@ -0,0 +1,6 @@
+let myHeight = 165;
+let myIdealWeight = (65*9)/10;
+let maxWeight = 65;
+let minWeight = (65*8)/10;
+
+console.log(myIdealWeight, maxWeight, minWeight);
